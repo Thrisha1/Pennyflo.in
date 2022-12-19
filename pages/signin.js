bf945f3efe8732bb3ecef354d41084ab99713e3b
Thrisha Kannan <98538342+Thrisha1@users.noreply.github.com>
2022-12-19 18:59:14 +0530

    changed

--- pages/signin.js
+++ pages/signin.js
@@ -1,10 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Head from "next/head";
 export default function Home() {
   return (
     <>
@@ -16,6 +10,7 @@ export default function Home() {
       </Head>
       <main>
         
-    </main>
+      </main>
     </>
-    )}
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'red': '#FF004F',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
