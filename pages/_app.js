54883e450eaec59dfb9ba6fa407b91aba467dd60
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2022-12-19 16:26:05 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import '../styles/globals.css'
+import React from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [initialRenderComplete, setInitialRenderComplete] = React.useState(false);
+  React.useEffect(() => {
+		// Updating a state causes a re-render
+		setInitialRenderComplete(true);
+	}, []);
+
+	// initialRenderComplete will be false on the first render and true on all following renders
+	if (!initialRenderComplete) 
+  {
+		// Returning null will prevent the component from rendering, so the content will simply be missing from
+		// the server HTML and also wont render during the first client-side render.
+		return null;
+	} 
+  else 
+  {
+    return <Component {...pageProps} />
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
@@ -16,106 +17,65 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
+          
           <div>
-            <a
+            <Link
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+              
+              
+            </Link>
           </div>
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+          <h1 class="text-6xl text-blue-400 p-2 font-bold">Penny Flo</h1>
           <div className={styles.thirteen}>
-            <Image
+            <p
               src="/thirteen.svg"
               alt="13"
               width={40}
               height={31}
               priority
             />
+            <h1 class="text-3xl text-blue">2.0</h1>
           </div>
         </div>
 
-        <div className={styles.grid}>
-          <a
+        <div class= "flex justify-center">
+          <Link
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
+            <Link href="/signin" className={inter.className}>
+              Sign In <span>-&gt;</span>
+            </Link>
             <p className={inter.className}>
               Find in-depth information about Next.js features and&nbsp;API.
             </p>
-          </a>
+          </Link>
 
-          <a
+          <Link
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
+              Get Started <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Learn about Next.js in an interactive course with&nbsp;quizzes!
             </p>
-          </a>
+          </Link>
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
+          
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          
         </div>
       </main>
     </>
